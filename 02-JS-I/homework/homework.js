a82ccd0f6f9cebda518118da0369589d4084758f
Pipituca95 <dianalorena_peralta@hotmail.com>
2022-06-03 21:17:46 -0300

    par

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -99,12 +99,16 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+  if( num % 2 === 0) { return true; 
+  } else { return false; }
 }
 
 function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
-  // Tu código:
+  // Tu código:}
+  if( num % 2 === 1) { return true; 
+  } else { return false; }
 }
 
 function elevarAlCuadrado(num) {
