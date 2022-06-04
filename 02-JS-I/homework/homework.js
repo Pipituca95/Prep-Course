c8008c3043e271668c68295633c5760a88477649
Pipituca95 <dianalorena_peralta@hotmail.com>
2022-06-04 19:10:58 -0300

    TareasTerminadas

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,7 +4,7 @@
 const nuevaString = "hola";
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = null;
+const nuevoNum = 3;
 
 // Crea una variable booleana:
 const nuevoBool = 5 - 3 === 2;
